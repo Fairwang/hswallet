05cd22114cbf9cb83b5f5614e80559feefbb9289
yumingxingxi127 <1273265323@qq.com>
2019-05-16 17:50:37 +0800

    m

--- swin_web/common/doexcel.py
+++ swin_web/common/doexcel.py
@@ -1,6 +1,10 @@
 #!/user/bin/python
 # -*- coding:utf-8 -*-
-import requests, xlrd, time, sys,xlwt,os
+
+#登录后台，查询订单，下载Excel，对数据进行统计，
+#需要修改查询时间和修改Excel的名称和第一行的内容
+
+import xlrd,xlwt
 
 def readExcel(file_path):
     '''''
@@ -26,12 +30,6 @@ def readExcel(file_path):
         fenxiexcel(case_list)
 
 def fenxiexcel(case_list):
-    res_flags = []
-    # 存测试结果的list
-    request_urls = []
-    # 存请求报文的list
-    responses = []
-    # 存返回报文的list
     #笔数和金额统计
     all_wechat_pens=0
     success_wechat_pens=0
@@ -39,8 +37,15 @@ def fenxiexcel(case_list):
     all_wechat_price=0
     all_sum=0
     success_wechat_price=0
-    yichangbili=0
     yichang_wechat_pens=0
+
+    all_ali_pens=0
+    success_ali_pens=0
+    fail_ali_pens=0
+    all_ali_price=0
+    success_ali_price=0
+    yichang_ali_pens=0
+
     for case in case_list:
         ''''' 
         先遍历excel中每一条case的值，然后根据对应的索引取到case中每个字段的值 
@@ -53,13 +58,13 @@ def fenxiexcel(case_list):
             status1=case[6]
         except Exception, e:
             return '测试用例格式不正确！%s' % e
-
         all_sum=all_sum+price
         if type==u"微信":
             all_wechat_price=all_wechat_price+price
             all_wechat_pens+=1
             if status==u"付款成功":
                 success_wechat_pens+=1
+                success_wechat_price=success_wechat_price+price
                 if status1==u"异常完成":
                     yichang_wechat_pens+=1
             else:
@@ -67,15 +72,23 @@ def fenxiexcel(case_list):
     print all_wechat_pens
     success=format(float(success_wechat_pens)/float(all_wechat_pens),".3f")
     yichangbili=format(float(yichang_wechat_pens)/float(all_wechat_pens),".3f")
+
+
     workbook=xlwt.Workbook(encoding="utf-8")
     worksheet=workbook.add_sheet("wechat")
     worksheet.write(1,0,all_wechat_pens)
+    worksheet.write(1,1,all_sum)
     worksheet.write(1,3,all_wechat_price)
-    worksheet.write(1,4,success_wechat_pens)
-    worksheet.write(1,5,fail_wechat_pens)
-    worksheet.write(1,6,yichang_wechat_pens)
-    worksheet.write(1,7,success)
-    worksheet.write(1,8,yichangbili)
+    worksheet.write(1,4, success_wechat_price)
+    worksheet.write(1,5,success_wechat_pens)
+    worksheet.write(1,6,fail_wechat_pens)
+    worksheet.write(1,7,yichang_wechat_pens)
+    worksheet.write(1,8,success)
+    worksheet.write(1,9,yichangbili)
+
+
+
+
     # print all_wechat_pens,all_wechat_price,success_wechat_pens,fail_wechat_pens,yichang_wechat_pens,success,yichangbili
     workbook.save("1111.xls")
 
